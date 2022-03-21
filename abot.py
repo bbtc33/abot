461c44af0f4aa748f68ee8667de939918f472718
Max <maxyin3872@gmail.com>
2022-03-21 10:27:28 -0400

    changed some stuff

--- abot.py
+++ abot.py
@@ -1,19 +1,19 @@
 import discord
 import re
+import os
+from discord.ext import commands
+from discord.ext.commands import Bot
+from os.path import exists
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
     if re.search('[aA]', message.content):
         total = message.content.count('a') + message.content.count('A')
         await message.channel.send('You used ' + str(total) + ' of the letter a in your message!')
 
-client.run('')
+token = open("discord_token", "r")
+discord_token = token.read()
+token.close()
+bot.run(discord_token)
